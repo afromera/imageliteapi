1dbf2283121ac9ed61029c1d5ba51e44e1c0d3a0
Alex Romera <af_romera@hotmail.com>
2026-03-31 23:33:51 -0300

    Initial commit

---
+++ src/test/java/romera/imageliteapi/ImageliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package romera.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
